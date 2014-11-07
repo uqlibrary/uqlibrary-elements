60cba1ea580afa83c846185334d3a095a43d40c5
Andrew Martlew <a.martlew@library.uq.edu.au>
2014-11-07 14:57:25 +1000

    Move to parent after installing components

--- bin/codeship.sh
+++ bin/codeship.sh
@@ -4,6 +4,7 @@ basedir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../ && pwd )
 cd $basedir
 
 bower install
+cd ..
 
 # Get a list of uplibrary-* components (excluding this one)
 components=$(ls -d */ | grep uqlibrary | grep -v elements)
