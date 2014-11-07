bdf4b07bd657ebc017d894b5d7f13317907448bf
Andrew Martlew <a.martlew@library.uq.edu.au>
2014-11-07 14:51:07 +1000

    Starting in current base dir

--- bin/codeship.sh
+++ bin/codeship.sh
@@ -6,7 +6,6 @@ cd $basedir
 bower install
 
 # Get a list of uplibrary-* components (excluding this one)
-cd ../..
 components=$(ls -d */ | grep uqlibrary | grep -v elements)
 
 # Run the tests for each component
