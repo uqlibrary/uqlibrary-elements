c5fa9850159d8fb66c506613d8fd52ba181f84c5
Andrew Martlew <a.martlew@library.uq.edu.au>
2014-11-11 10:33:07 +1000

    Run wct if tests exist

--- bin/codeship.sh
+++ bin/codeship.sh
@@ -12,7 +12,9 @@ components=$(ls -d */ | grep uqlibrary | grep -v elements)
 # Run the tests for each component
 for component in ${components[@]}; do
   cd $component
-  wct
+  if [ -d "test" ]; then
+    wct
+  fi
   cd ..
   cp -R $component "${component/uqlibrary-/}"
 done
