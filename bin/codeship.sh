2b7697e142d07c4ef4fa238aedb7df989752a3f4
Andrew Martlew <a.martlew@library.uq.edu.au>
2014-11-11 10:18:57 +1000

    Enabled wct

--- bin/codeship.sh
+++ bin/codeship.sh
@@ -12,7 +12,7 @@ components=$(ls -d */ | grep uqlibrary | grep -v elements)
 # Run the tests for each component
 for component in ${components[@]}; do
   cd $component
-  #wct
+  wct
   cd ..
   cp -R $component "${component/uqlibrary-/}"
 done
