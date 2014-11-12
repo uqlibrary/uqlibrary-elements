04b1637769d6864ba06727caeceeb038b95c6323
Andrew Martlew <a.martlew@library.uq.edu.au>
2014-11-13 07:24:15 +1000

    Vulcanize the index pages

--- bin/codeship.sh
+++ bin/codeship.sh
@@ -9,13 +9,15 @@ cd ..
 # Get a list of uplibrary-* components (excluding this one)
 components=$(ls -d */ | grep uqlibrary | grep -v elements)
 
-# Run the tests for each component
+# Run the tests for each component and vulcanize the index page
 for component in ${components[@]}; do
-  cd $component
+  cd ${component}
   #if [ -d "test" ]; then
   #  wct
   #fi
+  if [ -f "index.html" ]; then
+    vulcanize -o index.html index.html
+  fi
   cd ..
-  cp -R $component "${component/uqlibrary-/}"
+  cp -R ${component} "${component/uqlibrary-/}"
 done
-
