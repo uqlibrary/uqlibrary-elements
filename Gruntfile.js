42d3e7aa51367f63b336a83f6b11a73c7e4a9a71
Andrew Martlew <a.martlew@library.uq.edu.au>
2014-11-13 11:27:10 +1000

    Grunt S3 deployment

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    aws: grunt.file.readJSON('aws.json'),
+
+    invalidate_cloudfront: {
+      options: {
+        key: '<%= aws.AWSAccessKeyId %>',
+        secret: '<%= aws.AWSSecretKey %>',
+        region: '<%= aws.AWSRegion %>',
+        distribution: '<%= aws.CFDistribution %>'
+      },
+      production: {
+        files: [
+          {
+            expand: true,
+            cwd: '../',
+            src: ['**/*.html', '**/*.js', '**/*.css'],
+            filter: 'isFile',
+            dest: ''
+          }
+        ]
+      }
+    },
+    aws_s3: {
+      options: {
+        accessKeyId: '<%= aws.AWSAccessKeyId %>',
+        secretAccessKey: '<%= aws.AWSSecretKey %>',
+        region: '<%= aws.AWSRegion %>',
+        concurrency: 5
+      },
+      production: {
+        options: {
+          bucket: '<%= aws.S3Bucket %>',
+          params: { ContentEncoding: 'gzip' }
+        },
+        files: [
+          {
+            expand: true,
+            cwd: '../',
+            src: ['**'],
+            dest: ''
+          }
+        ]
+      }
+    }
+  });
+
+  grunt.registerTask('deploy', [
+    'aws_s3:production',
+    'invalidate_cloudfront'
+  ]);
+};
--- bin/codeship.sh
+++ bin/codeship.sh
@@ -2,7 +2,9 @@
 
 set -xe
 
-cd ~/src/github.com/uqlibrary/uqlibrary-elements/
+src=$(echo ~/src/github.com/uqlibrary/uqlibrary-elements)
+
+cd ${src}
 bower install
 cd ..
 
@@ -21,3 +23,21 @@ for component in ${components[@]}; do
   cd ..
   cp -R ${component} "${component/uqlibrary-/}"
 done
+
+# Use env vars to set AWS config
+set +x
+awsconfig="${src}/aws.json"
+sed -e s/"<AWSAccessKeyId>"/"${AWSSecretKey}"/g -i ${awsconfig} >/dev/null
+sed -e s/"<AWSSecretKey>"/"${AWSSecretKey}"/g -i ${awsconfig} >/dev/null
+sed -e s/"<S3Bucket>"/"${S3Bucket}"/g -i ${awsconfig} >/dev/null
+sed -e s/"<CFDistribution>"/"${CFDistribution}"/g -i ${awsconfig} >/dev/null
+sed -e s/"<AWSRegion>"/"${AWSRegion}"/g -i ${awsconfig} >/dev/null
+set -x
+
+# Gzip the files prior to uploading
+find . -type f ! -name '*.gz' -exec gzip "{}" \; -exec mv "{}.gz" "{}" \;
+
+cd ${src}
+grunt deploy
+
+rm -f ${awsconfig}
