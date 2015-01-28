8240e833cda5741aab69a5b012418dbe1cf764c7
Christiaan Kortekaas <ck@uq.edu.au>
2015-01-28 19:17:20 +1000

    Revulcanised with menu comments.. using comments, lol

--- 0.5.4/uqlibrary-menu/wct.conf.js
+++ 0.5.4/uqlibrary-menu/wct.conf.js
@@ -1,4 +1,14 @@
 module.exports = {
-  verbose: true,
-  browsers: ["chrome", "firefox"]
+  verbose: false,
+  browsers: ["chrome"],
+  plugins: {
+    sauce: {
+      browsers: [
+      {
+        browserName: "Safari",
+        platform: "OS X 10.10",
+        version: "8.0"
+      }
+  ]}
+}
 };
