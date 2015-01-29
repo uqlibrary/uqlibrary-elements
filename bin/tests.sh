bccf78429311dfde98e8dc54d53889fe757bfb7b
Christiaan Kortekaas <ck@uq.edu.au>
2015-01-29 22:36:08 +1000

    Added some debug to the tests runner to help in codeship sadness

--- bin/tests.sh
+++ bin/tests.sh
@@ -6,15 +6,16 @@ set +x
 #base=$(basename ${src})
 polymer=$(cat bower.json | grep "Polymer/polymer#" | cut -d'#' -f2 | cut -d'"' -f1)
 #tag=$(git describe --exact-match --tags HEAD)
-
+echo ${polymer}
 # Gzip component files
 cd ${polymer}
 
 # Get a list of uplibrary-* components (excluding this one)
 components=$(ls -d */ | grep uqlibrary | grep -v elements)
-
+echo $components
 # Run the tests for each component
 for component in ${components[@]}; do
+
   cd $component
   if [ -d "test" ]; then
     wct
