1771333165fe90ea3a8ef9f672fd4d1c3e488a05
Christiaan Kortekaas <ck@uq.edu.au>
2015-02-02 11:44:59 +1000

    Added branch conditional browser selections for sauce.sh

--- bin/sauce.sh
+++ bin/sauce.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
-wct --plugin sauce --sauce-username=${SAUCE_USERNAME} --sauce-access-key=${SAUCE_ACCESS_KEY} \
---sauce "OSX 10.9/safari@7.0" \
---sauce "Windows 8.1/chrome@39.0" \
---sauce "Windows 8.1/firefox@35.0" \
---sauce "Windows 8.1/internet explorer@11.0"
-#--sauce "linux/android@4.4" \
+branch=$(git rev-parse --abbrev-ref HEAD)
+case "$branch" in
+"master")
+  wct --plugin sauce --sauce-username=${SAUCE_USERNAME} --sauce-access-key=${SAUCE_ACCESS_KEY} \
+  --sauce "OSX 10.9/safari@7.0"
+  ;;
+"uat")
+  wct --plugin sauce --sauce-username=${SAUCE_USERNAME} --sauce-access-key=${SAUCE_ACCESS_KEY} \
+  --sauce "OSX 10.9/safari@7.0" \
+  --sauce "Windows 8.1/chrome@39.0"
+  ;;
+"staging")
+  wct --plugin sauce --sauce-username=${SAUCE_USERNAME} --sauce-access-key=${SAUCE_ACCESS_KEY} \
+  --sauce "OSX 10.9/safari@7.0" \
+  --sauce "Windows 8.1/chrome@39.0" \
+  --sauce "Windows 8.1/firefox@35.0" \
+  --sauce "Windows 8.1/internet explorer@11.0" \
+  --sauce "linux/android@4.4"
+  ;;
+"production")
+  wct --plugin sauce --sauce-username=${SAUCE_USERNAME} --sauce-access-key=${SAUCE_ACCESS_KEY} \
+  --sauce "OSX 10.9/safari@7.0" \
+  --sauce "Windows 8.1/chrome@39.0" \
+  --sauce "Windows 8.1/firefox@35.0" \
+  --sauce "Windows 8.1/internet explorer@11.0" \
+  --sauce "linux/android@4.4"
+  ;;
+esac
