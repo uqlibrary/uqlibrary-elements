5be69af924706cc14e3c16024b7922bdcf65e4b3
Christiaan Kortekaas <ck@uq.edu.au>
2015-02-04 09:51:52 +1000

    Turned sauce tests back on now that source of timeouts was found (scss compile fails)

--- bin/sauce.sh
+++ bin/sauce.sh
@@ -4,32 +4,28 @@ if [ -z $CI_BRANCH ]; then
 else
   branch=$CI_BRANCH
 fi
-
 case "$branch" in
 "master")
   wct --plugin local --local "chrome"
   ;;
 "uat")
-  wct --plugin local --local "chrome" --local "firefox"
-#  wct --plugin sauce --sauce-username=${SAUCE_USERNAME} --sauce-access-key=${SAUCE_ACCESS_KEY} \
-#  --sauce "OSX 10.9/safari@7.0" \
-#  --sauce "Windows 8.1/chrome@39.0"
+  wct --plugin sauce --sauce-username=${SAUCE_USERNAME} --sauce-access-key=${SAUCE_ACCESS_KEY} \
+  --sauce "OSX 10.9/safari@7.0" \
+  --sauce "Windows 8.1/chrome@39.0"
   ;;
 "staging")
-  wct --plugin local --local "chrome" --local "firefox"
-#  wct --plugin sauce --sauce-username=${SAUCE_USERNAME} --sauce-access-key=${SAUCE_ACCESS_KEY} \
-#  --sauce "OSX 10.9/safari@7.0" \
-#  --sauce "Windows 8.1/chrome@39.0" \
-#  --sauce "Windows 8.1/firefox@35.0" \
-#  --sauce "Windows 8.1/internet explorer@11.0"
+  wct --plugin sauce --sauce-username=${SAUCE_USERNAME} --sauce-access-key=${SAUCE_ACCESS_KEY} \
+  --sauce "OSX 10.9/safari@7.0" \
+  --sauce "Windows 8.1/chrome@39.0" \
+  --sauce "Windows 8.1/firefox@35.0" \
+  --sauce "Windows 8.1/internet explorer@11.0"
   ;;
 "production")
-  wct --plugin local --local "chrome" --local "firefox"
-#  wct --plugin sauce --sauce-username=${SAUCE_USERNAME} --sauce-access-key=${SAUCE_ACCESS_KEY} \
-#  --sauce "OSX 10.9/safari@7.0" \
-#  --sauce "Windows 8.1/chrome@39.0" \
-#  --sauce "Windows 8.1/firefox@35.0" \
-#  --sauce "Windows 8.1/internet explorer@11.0"
+  wct --plugin sauce --sauce-username=${SAUCE_USERNAME} --sauce-access-key=${SAUCE_ACCESS_KEY} \
+  --sauce "OSX 10.9/safari@7.0" \
+  --sauce "Windows 8.1/chrome@39.0" \
+  --sauce "Windows 8.1/firefox@35.0" \
+  --sauce "Windows 8.1/internet explorer@11.0"
   ;;
 *)
   echo "couldn't detect branch"
