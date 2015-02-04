a70d17826597c58fb9c5344c3a345421dc7fcfe0
Christiaan Kortekaas <ck@uq.edu.au>
2015-02-04 14:42:21 +1000

    Change how sauce gets branch on codeship in elements to be more accurate

--- bin/sauce.sh
+++ bin/sauce.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
-branch=$(git rev-parse --abbrev-ref HEAD)
+if [ -z $CI_BRANCH ]; then
+  branch=$(git rev-parse --abbrev-ref HEAD)
+else
+  branch=$CI_BRANCH
+fi
 case "$branch" in
 "master")
   wct --plugin local --local "chrome"
   ;;
 "uat")
-  wct --plugin local --local "chrome" --local "firefox"
-#  wct --plugin sauce --sauce-username=${SAUCE_USERNAME} --sauce-access-key=${SAUCE_ACCESS_KEY} \
-#  --sauce "OSX 10.9/safari@7.0" \
-#  --sauce "Windows 8.1/chrome@39.0"
+  wct --plugin sauce --sauce-username=${SAUCE_USERNAME} --sauce-access-key=${SAUCE_ACCESS_KEY} \
+  --sauce "OSX 10.9/safari@7.0" \
+  --sauce "Windows 8.1/chrome@39.0"
   ;;
 "staging")
-  wct --plugin local --local "chrome" --local "firefox"
-#  wct --plugin sauce --sauce-username=${SAUCE_USERNAME} --sauce-access-key=${SAUCE_ACCESS_KEY} \
-#  --sauce "OSX 10.9/safari@7.0" \
-#  --sauce "Windows 8.1/chrome@39.0" \
-#  --sauce "Windows 8.1/firefox@35.0" \
-#  --sauce "Windows 8.1/internet explorer@11.0"
+  wct --plugin sauce --sauce-username=${SAUCE_USERNAME} --sauce-access-key=${SAUCE_ACCESS_KEY} \
+  --sauce "OSX 10.9/safari@7.0" \
+  --sauce "Windows 8.1/chrome@39.0" \
+  --sauce "Windows 8.1/firefox@35.0" \
+  --sauce "Windows 8.1/internet explorer@11.0"
   ;;
 "production")
-  wct --plugin local --local "chrome" --local "firefox"
-#  wct --plugin sauce --sauce-username=${SAUCE_USERNAME} --sauce-access-key=${SAUCE_ACCESS_KEY} \
-#  --sauce "OSX 10.9/safari@7.0" \
-#  --sauce "Windows 8.1/chrome@39.0" \
-#  --sauce "Windows 8.1/firefox@35.0" \
-#  --sauce "Windows 8.1/internet explorer@11.0"
+  wct --plugin sauce --sauce-username=${SAUCE_USERNAME} --sauce-access-key=${SAUCE_ACCESS_KEY} \
+  --sauce "OSX 10.9/safari@7.0" \
+  --sauce "Windows 8.1/chrome@39.0" \
+  --sauce "Windows 8.1/firefox@35.0" \
+  --sauce "Windows 8.1/internet explorer@11.0"
   ;;
 *)
   echo "couldn't detect branch"
