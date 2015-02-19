24b2e07b38569c701b63bd8b414b7c0ebd80a990
Kit Brown-Watts <kbw@uq.edu.au>
2015-02-19 10:55:43 +1000

    Vulcanised changes

--- 0.5.4/uqlibrary-menu/test/testHelper.js
+++ 0.5.4/uqlibrary-menu/test/testHelper.js
@@ -2,8 +2,10 @@
  * Created by uqckorte on 28/01/15.
  */
 function checkMenu(readyMenu) {
+    console.log(readyMenu);
+    //console.log(readyMenu);
   var myName = readyMenu.shadowRoot.getElementById('name');
-  expect(myName.innerHTML).to.contain('John Doe');
+  expect(myName.innerHTML).to.contain('Mr. John Doe');
   var myId = readyMenu.shadowRoot.querySelector('paper-item#showHoursPage');
   expect(myId.innerHTML).to.contain('Hours');
 }
