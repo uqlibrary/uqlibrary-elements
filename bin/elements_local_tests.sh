288144f4f199552d53624215fcc55a1fe23a4ecb
Christiaan Kortekaas <ck@uq.edu.au>
2015-03-08 15:11:41 +1000

    Check for Codeship PIPE_NUM and PIPE_TOTAL environment variables and if exist split the tests

--- bin/elements_local_tests.sh
+++ bin/elements_local_tests.sh
@@ -13,15 +13,28 @@ cd ${polymer}
 # Get a list of uplibrary-* components (excluding this one).
 components=$(ls -d */ | grep uqlibrary | grep -v elements)
 echo $components
+let COUNTER=0
 # Run the tests for each component
 for component in ${components[@]}; do
-
-  cd $component
-  if [ -d "test" ]; then
-    echo $(pwd)
-    ../../bin/local_tests.sh
+  # if this is inside a codeship test pipeline, only run if its for this pipe number
+  if [ -z $PIPE_NUM && -z $PIPE_TOTAL ]; then
+    let COUNTER=COUNTER+1
+    if [[ $(( $i % $PIPE_TOTAL )) == $PIPE_NUM ]]; then
+      cd $component
+      if [ -d "test" ]; then
+        echo $(pwd)
+        ../../bin/local_tests.sh
+      fi
+      cd ..
+    fi
+  else
+    cd $component
+    if [ -d "test" ]; then
+      echo $(pwd)
+      ../../bin/local_tests.sh
+    fi
+    cd ..
   fi
-  cd ..
 done
 
 cd ..
