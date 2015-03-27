37a1b3374a077fbd56f6ffd44e1768ebe704d7c1
jannakha <j_malikova@yahoo.com>
2015-03-27 10:48:18 +1000

    revulcanized for uat

--- 0.5.4/uqlibrary-menu/test/testHelper.js
+++ 0.5.4/uqlibrary-menu/test/testHelper.js
@@ -2,10 +2,11 @@
  * Created by uqckorte on 28/01/15.
  */
 function checkMenu(readyMenu) {
-    console.log(readyMenu);
-    //console.log(readyMenu);
+
   var myName = readyMenu.shadowRoot.getElementById('name');
   expect(myName.innerHTML).to.contain('Mr. John Doe');
-  var myId = readyMenu.shadowRoot.querySelector('paper-item#showHoursPage');
+
+  var myId = readyMenu.shadowRoot.querySelector('paper-item#uqlibrary-hours');
   expect(myId.innerHTML).to.contain('Hours');
+
 }
