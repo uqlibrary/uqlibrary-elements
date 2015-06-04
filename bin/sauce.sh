b2ba5a78862c1b4303a7cb7c93a9f54674bf468d
Christiaan Kortekaas <ck@uq.edu.au>
2015-06-04 16:39:59 +1000

    updated safari to 8

--- bin/sauce.sh
+++ bin/sauce.sh
@@ -10,19 +10,19 @@ case "$branch" in
   ;;
 "uat")
   wct --plugin sauce --sauce-username=${SAUCE_USERNAME} --sauce-access-key=${SAUCE_ACCESS_KEY} \
-  --sauce "OSX 10.9/safari@7.0" \
+  --sauce "OSX 10.10/safari@8.0" \
   --sauce "Windows 8.1/chrome@43.0"
   ;;
 "staging")
   wct --plugin sauce --sauce-username=${SAUCE_USERNAME} --sauce-access-key=${SAUCE_ACCESS_KEY} \
-  --sauce "OSX 10.9/safari@7.0" \
+  --sauce "OSX 10.10/safari@8.0" \
   --sauce "Windows 8.1/chrome@43.0" \
   --sauce "Windows 8.1/firefox@37.0" \
   --sauce "Windows 8.1/internet explorer@11.0"
   ;;
 "production")
   wct --plugin sauce --sauce-username=${SAUCE_USERNAME} --sauce-access-key=${SAUCE_ACCESS_KEY} \
-  --sauce "OSX 10.9/safari@7.0" \
+  --sauce "OSX 10.10/safari@8.0" \
   --sauce "Windows 8.1/chrome@43.0" \
   --sauce "Windows 8.1/firefox@37.0" \
   --sauce "Windows 8.1/internet explorer@11.0"
